0e0b41be0b46b009730c6a97a96978f13b3d7a10
WichoToledo <Luisaierra83@gmail.com>
2019-06-06 01:22:58 -0600

    Pokedex

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ var P = angular.module("P",[])
 P.controller("Pokemon",function($scope,$rootScope,$http){
 	
 $scope.ListadoPokemon =[];
- for(var z-1;z<=100;++)(
+ for(var z=1;z<=100;z++)(
 
 	$http({
-	method;"GET"
+	method:"GET",
 	url: "https://pokeapi.co/api/v2/Pokemon/1"
-})then(function Datos(x) {
+}).then(function Datos(x) {
    console.log(x);
    $scope.ListadoPokemon.push(x);
-
-})  
+		})
+	});  	
