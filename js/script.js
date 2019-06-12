5f165f82a736f1fffae755dfe50b5f0bd9f9d8aa
WichoToledo <Luisaierra83@gmail.com>
2019-06-12 11:32:20 -0600

    Pokemon

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ var P = angular.module("P",[])
 P.controller("Pokemon",function($scope,$rootScope,$http){
 	
 $scope.ListadoPokemon =[];
- for(var z=1;z<=100;z++)(
+ for(var z=1;z<=100;z++){ 
 
 	$http({
 	method:"GET",
-	url: "https://pokeapi.co/api/v2/Pokemon/1"
+	url: "https://pokeapi.co/api/v2/pokemon/"+z
 }).then(function Datos(x) {
    console.log(x);
    $scope.ListadoPokemon.push(x);
 		})
-	});  	
+	}  	
+	});
